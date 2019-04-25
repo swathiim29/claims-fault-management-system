db961101597b73b65792c92f253c70a17505f88a
venmanda <mvswathi446@gmail.com>
2019-04-25 16:59:15 +0530

    uploading the project

---
+++ src/main/java/com/claims/fault/management/application/ClaimsFaultManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.claims.fault.management.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsFaultManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsFaultManagementSystemApplication.class, args);
+	}
+
+}
